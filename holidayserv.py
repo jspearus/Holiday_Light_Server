1aa7d5eba59a02263fd3df8013e532b5adce5523
jspearus <jspearus@gmail.com>
2021-10-21 17:42:23 -0500

    test code

--- holidayserv.py
+++ holidayserv.py
@@ -1,3 +1,32 @@
 import requests
+import websockets
+import _thread
+import asyncio
 import time
 import os
+import sys
+
+connected = set()
+
+sys.stdout.write("test runing \n")
+
+
+async def server(websocket, path):
+    # Register.
+    connected.add(websocket)
+    try:
+        async for message in websocket:
+            for conn in connected:
+                if conn != websocket:
+                    sys.stdout.write(str(type(conn)))
+                    await conn.send(f'Got a new MSG FOR YOU: {message}')
+                    sys.stdout.write(message + '\n')
+    finally:
+        # Unregister.
+        connected.remove(websocket)
+
+
+start_server = websockets.serve(server, "192.168.1.114", 5000)
+
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
